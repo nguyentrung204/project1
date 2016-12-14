291b8e9da45f5d02eff0fc3ba0645da4bedc0bed
trung <nguyenngoctrung204@gmail.com>
2016-12-14 18:57:21 +0700

    comit bootrap

---
+++ resources/views/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+		<link href="{!! asset('css/bootrap.min.css') !!}" media="all" rel="stylesheet" type="text/css" />
+		<script type="text/javascript" src="{!! asset('js/bootrap.min.js') !!}"></script>
+
+    </head>
+    <body>
+       Hello
+    </body>
+</html>
